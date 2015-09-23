687a059d210918cee738e2cb5f7bcc2ad064f116
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-09-23 20:34:28 +0200

    Implement fast enumeration.
    Fast enumeration works by keeping a doubly-linked list of used buckets.

--- hashtable.c
+++ hashtable.c
@@ -18,12 +18,19 @@ typedef struct hashtable_item_s {
   struct hashtable_item_s *next;
 } hashtable_item_t;
 
+typedef struct hashtable_rootItem_s {
+  hashtable_item_t item;
+  struct hashtable_rootItem_s *enumPrev;
+  struct hashtable_rootItem_s *enumNext;
+} hashtable_rootItem_t;
+
 struct hashtable_s {
   size_t cbuckets;
   hashtable_fcompare compare;
   hashtable_fhash hash;
   hashtable_ffree free;
-  hashtable_item_t *buckets;
+  hashtable_rootItem_t *enumHead;
+  hashtable_rootItem_t *buckets;
 };
 
 
@@ -46,23 +53,21 @@ hashtable_t *hashtable_make(size_t cbuckets, hashtable_fcompare c, hashtable_fha
 
 
 void hashtable_free(hashtable_t *ht) {
-  size_t i;
+  hashtable_rootItem_t *thisHead;
   hashtable_item_t *this, *next;
   
-  for (i=0; i<ht->cbuckets; i++) {
-    this = &(ht->buckets[i]);
-    next = this->next;
-    if (this->item && ht->free)
-      ht->free(this->item);
-      
-    while (next) {
-      this = next;
+  thisHead = ht->enumHead;
+  while (thisHead) {
+    this = thisHead->item.next;
+    while (this) {
       next = this->next;
-      if (ht->free)
-        ht->free(this->item);
+      ht->free(this->item);
       free(this);
+      this = next;
     }
+    thisHead = thisHead->enumNext;
   }
+  
   free(ht->buckets);
   free(ht);
 }
@@ -71,18 +76,26 @@ void hashtable_free(hashtable_t *ht) {
 void hashtable_insert(hashtable_t *ht, void *item) {
   hashtable_hash_t hash;
   size_t ph;
+  hashtable_rootItem_t *rthis;
   hashtable_item_t *this, *next;
   
   if (!item) return;
   
   hash = ht->hash(item);
   ph = hash % ht->cbuckets;
-  this = &(ht->buckets[ph]);
+  rthis = &(ht->buckets[ph]);
+  this = &(rthis->item);
   if (this->item) {
     next = calloc(1, sizeof(hashtable_item_t));
     next->next = this->next;
     this->next = next;
     this = next;
+  } else {
+    rthis->enumPrev = NULL;
+    rthis->enumNext = ht->enumHead;
+    if (ht->enumHead)
+      ht->enumHead->enumPrev = rthis;
+    ht->enumHead = rthis;
   }
   
   this->fullhash = hash;
@@ -97,7 +110,7 @@ void *hashtable_search(hashtable_t *ht, void *key) {
   
   hash = ht->hash(key);
   ph = hash % ht->cbuckets;
-  this = &(ht->buckets[ph]);
+  this = &(ht->buckets[ph].item);
   if (!(this->item))
     return NULL;
   
@@ -113,11 +126,13 @@ void *hashtable_search(hashtable_t *ht, void *key) {
 int hashtable_remove(hashtable_t *ht, void *key) {
   hashtable_hash_t hash;
   size_t ph;
+  hashtable_rootItem_t *rthis;
   hashtable_item_t *prev, *this;
   
   hash = ht->hash(key);
   ph = hash % ht->cbuckets;
-  this = &(ht->buckets[ph]);
+  rthis = &(ht->buckets[ph]);
+  this = &(rthis->item);
   if (!(this->item))
     return 0;
   
@@ -128,10 +143,18 @@ int hashtable_remove(hashtable_t *ht, void *key) {
         prev->next = this->next;
         free(this);
       } else {
-        if (this->next)
+        if (this->next) {
           *this = *(this->next);
-        else
+        } else {
+          if (!rthis->enumPrev)
+            ht->enumHead = rthis->enumNext;
+          else
+            rthis->enumPrev->enumNext = rthis->enumNext;
+          if (rthis->enumNext)
+            rthis->enumNext->enumPrev = rthis->enumPrev;
+            
           memset(this, 0, sizeof(hashtable_item_t));
+        }
       }
       return 1;
     }
@@ -146,35 +169,34 @@ int hashtable_remove(hashtable_t *ht, void *key) {
 
 struct hashtable_enum_s {
   hashtable_t *ht;
-  ssize_t i;
+  hashtable_rootItem_t *thisHead;
   hashtable_item_t *this;
 };
 
 hashtable_enum_t *hashtable_enumerate(hashtable_enum_t *s, hashtable_t *ht, void **item) {
   if (s == NULL) {
+    if (!(ht->enumHead))
+      return NULL;
+      
     s = calloc(1, sizeof(hashtable_enum_t));
     s->ht = ht;
-    s->i = -1;
-  }
-  if (!item)
+    s->thisHead = ht->enumHead;
+    s->this = &(s->thisHead->item);
+  } else if (!item) {
     goto finish;
-    
-  if (ht)
-    assert(ht == s->ht);
-  else
-    ht = s->ht;
+  } else {
+    if (ht)
+      assert(ht == s->ht);
+    else
+      ht = s->ht;
   
-  if (s->this)
     s->this = s->this->next;
-    
-  if (!(s->this)) {
-    for (s->i++; s->i < ht->cbuckets; s->i++) {
-      if (ht->buckets[s->i].item)
-        break;
+    if (!(s->this)) {
+      s->thisHead = s->thisHead->enumNext;
+      if (!(s->thisHead))
+        goto finish;
+      s->this = &(s->thisHead->item);
     }
-    if (s->i >= ht->cbuckets)
-      goto finish;
-    s->this = &(ht->buckets[s->i]);
   }
   
   if (item)
